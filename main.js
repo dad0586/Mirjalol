04dacaa92846d06aacd8f308eba617b40ec8af24
otaboy <rakhmonovotaboy0586@gmail.com>
2025-02-02 16:49:54 +0500

    #3 add clas list

--- main.js
+++ main.js
@@ -1,9 +1,7 @@
-const ismlar = ['ahror', 'danyor', 'farxod', 'aziz'];
-const newName = [];
+const para = document.querySelectorAll('p')
+console.log(para);
+para.forEach((text) =>{
+    if (text.textContent.includes('succes') )
+    text.classList.add('succes ')
 
-ismlar.forEach (function(ism){
-    const newIsm = ism.charAt().toUpperCase() + ism.slice(1).toLowerCase() + "bek"
-    newName.push(newIsm);  
 })
-
-console.log(newName);
