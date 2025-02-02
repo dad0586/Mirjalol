b933409653340ada90da66f12d7bc976b2dd3d2b
otaboy <rakhmonovotaboy0586@gmail.com>
2025-02-02 11:46:01 +0500

    #2 funksiya turlari

--- main.js
+++ main.js
@@ -1,7 +1,29 @@
+ // function declaration
+ 
+ fDeclation (); // hohlagan  joyimzda chaqirib ishlatishimz mumkin
+
  function fDeclation () {
     console.log("Hello function declation");
  }
+  fDeclation ();
+  fDeclation ();
+  fDeclation ();
+ 
+// function expression
+    
+ // bunda tepada chaqirib bolmaydi  fExpression ();
+ const fExpression = function () {
+    console.log("Hello function expression");
+ }
+  fExpression ();
+  fExpression ();
+  fExpression ();
 
-  fDeclation ();
-  fDeclation ();
-  fDeclation ();
+// arrow function
+ // buni ham tepada chaqirib bolmaydi
+const fArrow = () => {
+    console.log("Hello Arrow function ");
+}
+fArrow();
+fArrow();
+fArrow();
