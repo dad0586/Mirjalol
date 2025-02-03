e42c975fda6de5385d0fa0b043b4fc8d261a2525
otaboy <rakhmonovotaboy0586@gmail.com>
2025-02-03 15:59:28 +0500

    e_target

--- main.js
+++ main.js
@@ -3,3 +3,15 @@ button.addEventListener( 'click', () => {
     console.log("you clicked me");
     
 })
+
+
+const items = document.querySelectorAll('li')
+// console.log(items);
+items.forEach((item) =>{
+    item.addEventListener('click' , (e) => {
+        // console.log(item);
+        item.style.textDecoration = 'line-through'
+        e.target.style.opacity = '0.8'
+        
+    })
+})
