e7aee2d6e57bdb661e68970a38b4d9435ebe66ce
otaboy <rakhmonovotaboy0586@gmail.com>
2025-02-03 15:12:30 +0500

    click code

--- main.js
+++ main.js
@@ -1,10 +1,5 @@
-const para = document.querySelectorAll('p');
-para.forEach((text) => {
-    if (text.textContent.includes('succes')) { 
-        text.classList.add('succes');
-    }
-    if (text.textContent.includes('error')) {
-        text.classList.add('error');
-    }
-});
- 
+const button = document.querySelector('button')
+button.addEventListener( 'click', () => {
+    console.log("you clicked me");
+    
+})
