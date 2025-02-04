ae3ca31ff310ccfc7892c43f64a48ee03d02680e
otaboy <rakhmonovotaboy0586@gmail.com>
2025-02-04 13:22:08 +0500

    #1 04.02.2025 Array filter method

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
-document.addEventListener('keydown', (e) => {
-    console.log('code', e.code);
-    console.log('key', e.key);
-    console.log('keycode', e.keyCode);
-    
-} )
+const movies = [
+    {name: 'Avatar' , year: 2022, rating: 9 },
+    {name: 'Bable' , year: 2010, rating: 5 },
+    {name: 'Shrek' , year: 2009, rating: 9 },
+    {name: 'Avatar 2' , year: 2024, rating: 7 },
+    {name: 'Mufassa ' , year: 2024, rating: 8 }
+]
+
+const filterMovies = movies.filter ((movie) => {
+    return movie.year < 2015;
+})
+console.log(filterMovies);
