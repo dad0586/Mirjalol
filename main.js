4b6ddc92f3e9d7b2bc95611d28e6a51dd791ae92
otaboy <rakhmonovotaboy0586@gmail.com>
2025-02-04 12:54:52 +0500

    #1 04.02.2025 keyEvent

--- main.js
+++ main.js
@@ -1,30 +1,6 @@
-const ul = document.querySelector('ul')
-const button = document.querySelector('button')
-button.addEventListener( 'click', () => {
-    
-    const li = document.createElement('li')
-    li.textContent = 'Mirjalol  ✌️'
-    ul.appendChild(li)
-    
-})
-
-
-const items = document.querySelectorAll('li')
-// console.log(items);
-items.forEach((item) =>{
-    item.addEventListener('click' , (e) => {
-        // console.log(item);
-        // item.style.textDecoration = 'line-through'
-        // e.target.style.opacity = '0.8'
-        // item.remove();
-        
-    })
-})
-
-ul.addEventListener('click', (e) => {
-    if(e.target.nodeName == 'LI'){
-        e.target.remove();
-    }
+document.addEventListener('keydown', (e) => {
+    console.log('code', e.code);
+    console.log('key', e.key);
+    console.log('keycode', e.keyCode);
     
 } )
-
