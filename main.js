315df9a8709363e9d7e18d1ad619fe909ea49691
otaboy <rakhmonovotaboy0586@gmail.com>
2025-02-04 20:40:41 +0500

    #1 04.02.2025 setTimeout

--- main.js
+++ main.js
@@ -1,27 +1,15 @@
-const movies = [
-    { name: 'Avatar', year: 2022, rating: 9 },
-    { name: 'Bable', year: 2010, rating: 5 },
-    { name: 'Shrek', year: 2009, rating: 9 },
-    { name: 'Avatar 2', year: 2024, rating: 7 },
-    { name: 'Mufassa ', year: 2024, rating: 8 }
-]
+setTimeout(() => {
+    console.log('hiiiiii');
 
-// const mapMovies = movies.map((movie) => {
-//     return { name: movie.name, year: movie.year, rating: movie.rating + 1 }
-// })
-// console.log(mapMovies);
-// console.log(movies);
+}, 3000)
 
 
-const sortedMovies = movies.sort((a, b) => {
-    return a.rating - b.rating;
-})
-console.log('sortedMovies');
-console.log(sortedMovies);
 
-const age = 15;
-const year = 2000;
-age > 20 ? console.log("togri") : console.log('xato');
-year > 1996 && console.log('togri tanlov');
+function callMe() {
+    alert('Hiii Javascript')
+}
 
 
+setTimeout(() => {
+    callMe ()
+}, 2000)
